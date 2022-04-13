c83dacea899357b8f53a72a83cfa2587e9aa8d54
kqf <q.dev.null@gmail.com>
2022-04-13 07:26:57 +0200

    Add misc (#10)
    * Add a proxy file
    
    * Generalize to all files

---
+++ misc/proxy.sh
@@ -0,0 +1,23 @@
+function enable-proxy {
+    local withports=$(ps ax | grep cntlm | grep localhost)
+
+    # Start the cntlm if no one has started yet
+    [ -z "$withports" ] && rm -f ~/.cntlm-*.pid && cntlm-start
+
+
+    local port=$(
+        ps ax |
+        grep cntlm |
+        grep localhost |
+        tail -n 1 |
+        tr -s ' ' |
+        cut -d' ' -f16
+    )
+    echo "An exposed port found ${port}. Exporting the variables ..."
+    export http_proxy=http://localhost:${port}/
+    export https_proxy=http://localhost:${port}/
+    echo "Exported variables:"
+    echo -e "\thttp_proxy=${http_proxy}"
+    echo -e "\thttps_proxy=${https_proxy}"
+    echo "Done."
+}
