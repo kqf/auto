fc0affb2b40747c7ded584b31112c21372abf071
Sasha <q.dev.null@gmail.com>
2022-04-15 21:09:23 +0200

    Add the launch script

--- aws/ec2.sh
+++ aws/ec2.sh
@@ -9,3 +9,15 @@ function aws-instance-id() {
 function ec2-instance() {
     aws-instance-id $1 | xargs aws ec2 $2-instances --instance-ids
 }
+
+function ec2-launch-instance()
+{
+    # This image corresponds to ubuntu 20.04
+    aws ec2 run-instances \
+        --image-id ami-04505e74c0741db8d \
+        --count 1 \
+        --instance-type t2.xlarge \
+        --key-name existing-key-pair-name \
+        --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=meaningful-name}]' \
+        --block-device-mapping '[ {"DeviceName": "/dev/sda1", "Ebs": {"VolumeSize": 128}} ]'
+}
