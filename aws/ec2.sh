ffc5b1664c2ced261f49a36d34564ca40c0f58f3
Sasha <q.dev.null@gmail.com>
2022-04-15 21:25:37 +0200

    Rename the instance launch

--- aws/ec2.sh
+++ aws/ec2.sh
@@ -6,7 +6,7 @@ function aws-instance-id() {
     aws-instances-list | grep "$1" | awk '{print $1}'
 }
 
-function ec2-instance() {
+function aws-instance() {
     aws-instance-id $1 | xargs aws ec2 $2-instances --instance-ids
 }
 
