77ba381ae42edfaa6f382479e09aa7127678b0ff
Sasha <q.dev.null@gmail.com>
2022-04-15 20:10:25 +0200

    Add EC2 interface

--- aws/ec2.sh
+++ aws/ec2.sh
@@ -5,3 +5,7 @@ function aws-instances-list() {
 function aws-instance-id() {
     aws-instances-list | grep "$1" | awk '{print $1}'
 }
+
+function ec2-instance() {
+    aws-instance-id $1 | xargs aws ec2 $2-instances --instance-ids
+}
